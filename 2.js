19d32e35581eddf99d16ee03ffe9a2f65d7284d4
Sergio Prada <chechopradaster@gmail.com>
2018-03-03 17:48:51 -0500

    clean up

--- 2.js
+++ 2.js
@@ -4,10 +4,10 @@ const Mnemonic = require('bitcore-mnemonic');
 const bip39 = require('bip39');
 const testnet = bitcoin.networks.testnet
 
+// FIRST challenge
 // Generated from bip39
 const mnemonic = new Mnemonic('wash indoor drip legend roof noodle unable item sausage equal nurse wood');
 const seed = bip39.mnemonicToSeedHex(mnemonic)
-
 const xpriv ='tprv8ZgxMBicQKsPfPBSN58S9sLnLsGJFSPChTmwcdLQwW2oKj4WSFiNULdeuE2wrf7hEwP7nRFytjm2DhyfVuQVas22SibmQzhfJbE8r3DecdE';
 
 var root = bitcoin.HDNode.fromSeedHex(seed, testnet);
@@ -21,22 +21,26 @@ const main = async () => {
     const ecl = new ElectrumCli(50001, '18.221.223.44', 'tcp')
     await ecl.connect() // connect(promise)
     ecl.subscribe.on('blockchain.headers.subscribe', (v) => console.log(v))
-    try{
+
+    try {
+
+        // SECOND challenge
         const balance = await ecl.blockchainAddress_getBalance(child1.getAddress())
         console.log('balance', balance)
 
+        // THIRD challenge
         // blockchainAddress_listunspent
         const list = await ecl.blockchainAddress_listunspent(child1.getAddress())
         const tx_hash = list[0].tx_hash;
 
-        var SERGIO = bitcoin.ECPair.fromWIF(WIF, testnet);
-        var tx = new bitcoin.TransactionBuilder(testnet);
+        const KEY = bitcoin.ECPair.fromWIF(WIF, testnet);
+        const tx = new bitcoin.TransactionBuilder(testnet);
 
         tx.addInput(tx_hash, 0)
         tx.addOutput('2N1eoNZQregurjsp3Kij2J9uZ5UQTPcMNtx', 120000000) // FAUCET ADDRESS
         tx.addOutput(child1.getAddress(), 500)
 
-        tx.sign(0, SERGIO)
+        tx.sign(0, KEY)
 
         console.log(tx.build().toHex());
 
